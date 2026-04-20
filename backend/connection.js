e072d3c2aec9383ec1ffb1909bcc8996c9a27dac
Sarvan-Singh <sarvan1724.becse24@chitkara.edu.in>
2026-04-20 10:11:41 +0530

    fix(security): moved MongoDB URI to environment variables to fix credential leak

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://server2028sarvan_db_user:SvEVMex8pFkbnTCo@cluster1.dobsek9.mongodb.net/?appName=Cluster1')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
