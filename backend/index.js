a6937d5f7414ae11c0f636b3e713c678f08cdebc
Dishu1871 <deepanshugoyal1264@gmail.com>
2026-04-25 22:08:20 +0530

    history.js route and controller in backend

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,15 @@ const medicineRoutes = require("./Routes/medicine");
 const hospitalRoutes = require("./Routes/nearByHospital")
 const notificationRoutes = require("./Routes/notification");
 const gallaryRoutes = require("./Routes/gallary");
-
+const historyRoutes = require("./Routes/history");
 app.use("/api/auth", userRoutes);
 app.use("/api/facility", facilityRoutes);
 app.use("/api/medicine", medicineRoutes);
 app.use("/api/hospital", hospitalRoutes);
 app.use("/api/notification", notificationRoutes);
 app.use("/api/gallary", gallaryRoutes);
+app.use("/api/history", historyRoutes);
+
 
 
 
