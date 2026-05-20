76d09ec6b607f4f892c3c8c727649aa6d9b7ed3a
Sarvan-Singh <sarvan1724.becse24@chitkara.edu.in>
2026-05-20 17:19:53 +0530

    Did Conditional Routing and give role base access to all ie -> Students, Admin and Staff

--- frontendDispensary/src/App.jsx
+++ frontendDispensary/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route ,Navigate} from 'react-router-dom'
 import Home from './pages/Home/home'
 import './App.css'
 import Header from './components/Header/header'
@@ -18,7 +18,10 @@ import GlobalLoader from './components/GlobalLoader/globalLoader'
 function App() {
   const [count, setCount] = useState(0)
   const [loader,setLoader]=useState(false);
-  const [isLogin,setIsLogin]=useState(localStorage.getItem('isLogin'));
+  const [isLogin,setIsLogin]=useState(localStorage.getItem('isLogin'));//as we also set in localStorage that weather user login or not
+
+  let role = localStorage.getItem("userInfo")? JSON.parse(localStorage.getItem("userInfo")).role:null; //to get role of user from local storage to restrict access of pages according to role
+  let id = localStorage.getItem("userInfo")? JSON.parse(localStorage.getItem("userInfo")).id:null;//to get id of user from local storage to restrict access of pages according to role
   const handleLogin=(value)=>{
     setIsLogin(value);
   }
@@ -33,17 +36,18 @@ function App() {
       <Header isLogin={isLogin}handleLogin={handleLogin} showLoader={showLoader} hideLoader={hideLoader} />
       <Routes>
         <Route path='/' element={<Home showLoader={showLoader} hideLoader={hideLoader}  />} />
-        <Route path='/login' element={<Login handleLogin={handleLogin} showLoader={showLoader} hideLoader={hideLoader} />} />
+        <Route path='/login' element={isLogin ? role === "student" ? <Navigate to={`/student/${id}`} /> : <Navigate to={'/admin/dashboard'} /> : <Login handleLogin={handleLogin} showLoader={showLoader} hideLoader={hideLoader} />} />
+           {/* ///role based redirection to dashboard after login */}
         <Route path='/stock' element={<Stock showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/dashboard' element={<AdminDashboard showLoader={showLoader} hideLoader={hideLoader}/>} />
-        <Route path='/admin/register-student' element={<RegisterStudent showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/manage-medicine' element={<ManageMedicine showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/record' element={<Record showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/facility' element={<Facility showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/nearByHospital' element={<NearByHospital showLoader={showLoader} hideLoader={hideLoader} />} />
-        <Route path='/admin/gallary' element={<AdminGallary showLoader={showLoader} hideLoader={hideLoader} />} />
+        <Route path='/admin/dashboard' element={isLogin && role!=='student' ? <AdminDashboard showLoader={showLoader} hideLoader={hideLoader}/> : <Navigate to={'/'} />} />
+        <Route path='/admin/register-student' element={isLogin && role!=='student' ? <RegisterStudent showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
+        <Route path='/admin/manage-medicine' element={isLogin && role!=='student' ? <ManageMedicine showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
+        <Route path='/admin/record' element={isLogin && role!=='student' ? <Record showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
+        <Route path='/admin/facility' element={isLogin && role!=='student' ? <Facility showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
+        <Route path='/admin/nearByHospital' element={isLogin && role!=='student' ? <NearByHospital showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
+        <Route path='/admin/gallary' element={isLogin && role!=='student' ? <AdminGallary showLoader={showLoader} hideLoader={hideLoader} /> : <Navigate to={'/'} />} />
 
-        <Route path='/student/:id' element={<StudentDashboard showLoader={showLoader} hideLoader={hideLoader}/>}></Route>
+        <Route path='/student/:id' element={isLogin && role === 'student'?<StudentDashboard showLoader={showLoader} hideLoader={hideLoader}/>: <Navigate to={'/'} />} ></Route>
       </Routes>
       <Footer />
 
